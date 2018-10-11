b7371933734d3219e70be8a4f54025f6891b825d
jicata <jitza_nest@yahoo.com>
2018-10-11 16:27:29 +0300

    Added base View class

---
+++ src/SIS.Framework/Views/View.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using SIS.Framework.ActionsResults.Contracts;
+
+namespace SIS.Framework.Views
+{
+    public class View : IRenderable
+    {
+        private readonly string fullyQualifiedTemplateName;
+
+        public View(string fullyQualifiedTemplateName)
+        {
+            this.fullyQualifiedTemplateName = fullyQualifiedTemplateName;
+        }
+
+        private string ReadFile()
+        {
+            if (!File.Exists(this.fullyQualifiedTemplateName))
+            {
+                throw new FileNotFoundException($"View does not exist at {fullyQualifiedTemplateName}");
+            }
+
+            return File.ReadAllText(this.fullyQualifiedTemplateName);
+        }
+
+        public string Render()
+        {
+            var fullHtml = this.ReadFile();
+            return fullHtml;
+        }
+    }
+}

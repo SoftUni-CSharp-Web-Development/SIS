29fca9fd38603a7026a5fe3c7ac4b9265e634405
jicata <jitza_nest@yahoo.com>
2018-10-11 16:27:17 +0300

    Added ControllerUtilities

---
+++ src/SIS.Framework/Utilities/ControllerUtilities.cs
@@ -0,0 +1,21 @@
+﻿using SIS.Framework.Controllers;
+
+namespace SIS.Framework.Utilities
+{
+    public static class ControllerUtilities
+    {
+        public static string GetControllerName(object controller) =>
+            controller
+                .GetType()
+                .Name
+                .Replace(MvcContext.Get.ControllerSuffix, string.Empty);
+
+        public static string GetViewFullyQualifiedName(
+            string controllerName,
+            string viewName) =>
+            string.Format("{0}//{1}//{2}",
+                MvcContext.Get.ViewsFolder,
+                controllerName,
+                viewName);
+    }
+}

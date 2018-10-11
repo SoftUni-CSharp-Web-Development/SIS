c00b06bf15381f503eaf94e9a2a5e7bba3f8c98a
jicata <jitza_nest@yahoo.com>
2018-10-11 17:38:59 +0300

    Added ControllerRouter

---
+++ src/SIS.Framework/Routers/ControllerRouter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using SIS.Framework.ActionsResults.Base;
+using SIS.Framework.Attributes.Methods;
+using SIS.Framework.Attributes.Methods.Base;
+using SIS.Framework.Controllers;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.WebServer.Api.Contracts;
+
+namespace SIS.Framework.Routers
+{
+    public class ControllerRouter : IHttpHandler
+    {
+        public IHttpResponse Handle(IHttpRequest request)
+        {
+            var controllerName = string.Empty;
+            var actionName = string.Empty;
+            var requestMethod = request.RequestMethod.ToString();
+
+            if (request.Url == "/")
+            {
+                controllerName = "Home";
+                actionName = "Index";
+            }
+            else
+            {
+                var requestUrlSplit = request.Url.Split(
+                    "/",
+                    StringSplitOptions.RemoveEmptyEntries);
+                
+                controllerName = requestUrlSplit[0];
+                actionName = requestUrlSplit[1];
+            }
+
+            // var result = new Controller.Action();
+            // handle => result
+
+            //Controller
+            var controller = this.GetController(controllerName, request);
+
+            //Action
+            var action = this.GetAction(requestMethod, controller, actionName);
+
+            if (controller == null || action == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return this.PrepareResponse(controller, action);
+        }
+
+        private IHttpResponse PrepareResponse(Controller controller, MethodInfo action)
+        {
+            IActionResult actionResult = (IActionResult)action.Invoke(controller, null);
+            string invocationType = actionResult.Invoke();
+            return null;
+        }
+
+
+        private MethodInfo GetAction(
+            string requestMethod,
+            Controller controller,
+            string actionName)
+        {
+            var actions = this
+                .GetSuitableMethods(controller, actionName)
+                .ToList();
+
+            if (!actions.Any())
+            {
+                return null;
+            }
+
+            foreach (var action in actions)
+            {
+                var httpMethodAttributes = action
+                    .GetCustomAttributes()
+                    .Where(ca => ca is HttpMethodAttribute)
+                    .Cast<HttpMethodAttribute>()
+                    .ToList();
+
+                if (!httpMethodAttributes.Any() &&
+                    requestMethod.ToLower() == "get")
+                {
+                    return action;
+                }
+
+                foreach (var httpMethodAttribute in httpMethodAttributes)
+                {
+                    if (httpMethodAttribute.IsValid(requestMethod))
+                    {
+                        return action;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private IEnumerable<MethodInfo> GetSuitableMethods(
+            Controller controller,
+            string actionName)
+        {
+            if (controller == null)
+            {
+                return new MethodInfo[0];
+            }
+
+            return controller
+                .GetType()
+                .GetMethods()
+                .Where(mi => mi.Name.ToLower() == actionName.ToLower());
+        }
+
+        private Controller GetController(string controllerName, IHttpRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(controllerName))
+            {
+                return null;
+            }
+
+     
+            var fullyQualifiedControllerName = string.Format("{0}.{1}.{2}{3}, {0}",
+                MvcContext.Get.AssemblyName,
+                MvcContext.Get.ControllersFolder,
+                controllerName,
+                MvcContext.Get.ControllerSuffix);
+
+            var controllerType = Type.GetType(fullyQualifiedControllerName);
+            var controller = (Controller)Activator.CreateInstance(controllerType);
+            return controller;
+        }
+    }
+}

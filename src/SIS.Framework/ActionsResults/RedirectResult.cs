de87128b219dbf38a321802ab08f9a83ba10ca02
jicata <jitza_nest@yahoo.com>
2018-10-11 15:19:02 +0300

    Added concrete IActionResult implementations

---
+++ src/SIS.Framework/ActionsResults/RedirectResult.cs
@@ -0,0 +1,17 @@
+﻿using SIS.Framework.ActionsResults.Contracts;
+
+namespace SIS.Framework.ActionsResults
+{
+    public class RedirectResult : IRedirectable
+    {
+        public RedirectResult(string redirectUrl)
+        {
+            this.RedirectUrl = redirectUrl;
+        }
+
+        public string RedirectUrl { get; }
+
+        public string Invoke() =>
+            this.RedirectUrl;
+    }
+}
---
+++ src/SIS.Framework/ActionsResults/ViewResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SIS.Framework.ActionsResults.Contracts;
+
+namespace SIS.Framework.ActionsResults
+{
+    public class ViewResult : IViewable
+    {
+        public ViewResult(IRenderable view)
+        {
+            this.View = view;
+        }
+
+        public IRenderable View { get; set; }
+
+        public string Invoke() =>
+            this.View.Render();
+    }
+}

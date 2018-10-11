31fb189445721f7f0c4c1473463d75a9e216c587
jicata <jitza_nest@yahoo.com>
2018-10-11 15:19:31 +0300

    Added IHttphandler interface and implementation

---
+++ src/SIS.WebServer/Api/Contracts/IHttpHandler.cs
@@ -0,0 +1,10 @@
+﻿using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+
+namespace SIS.WebServer.Api.Contracts
+{
+    public interface IHttpHandler
+    {
+        IHttpResponse Handle(IHttpRequest request);
+    }
+}

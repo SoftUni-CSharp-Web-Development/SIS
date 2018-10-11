45e1e3773fff074df10f8b7a23ea11227cd2b2b0
jicata <jitza_nest@yahoo.com>
2018-10-11 15:19:14 +0300

    Added MvcContext

---
+++ src/SIS.Framework/MvcContext.cs
@@ -0,0 +1,22 @@
+﻿namespace SIS.Framework
+{
+    public class MvcContext
+    {
+        private static MvcContext Instance;
+
+        private MvcContext() { } 
+
+        public static MvcContext Get => Instance ?? (Instance = new MvcContext());
+
+        public string AssemblyName { get; set; }
+
+        public string ControllerSuffix { get; set; } = "Controller";
+
+        public string ControllersFolder { get; set; } = "Controllers";
+
+        public string ViewsFolder { get; set; } = "Views";
+
+        public string ModelsFolder { get; set; } = "Models";
+
+    }
+}

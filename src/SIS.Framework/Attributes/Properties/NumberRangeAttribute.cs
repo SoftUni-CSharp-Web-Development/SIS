52366cb234826136f00245cca49d0f82952dce60
jicata <jitza_nest@yahoo.com>
2018-10-15 17:38:07 +0300

    Added NumberRangeAttribute

---
+++ src/SIS.Framework/Attributes/Properties/NumberRangeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SIS.Framework.Attributes.Properties
+{
+    public class RegexAttribute : ValidationAttribute
+    {
+        private readonly string pattern;
+
+        public RegexAttribute(string pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return Regex.IsMatch(value.ToString(), pattern);
+        }
+    }
+}

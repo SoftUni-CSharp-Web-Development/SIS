96977deff1d5232d979f0aaff4ef4264d725dc19
jicata <jitza_nest@yahoo.com>
2018-10-15 15:07:47 +0300

    Extracted logic for starting up the server

---
+++ src/SIS.Framework/MvcEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using SIS.WebServer;
+
+namespace SIS.Framework
+{
+    public class MvcEngine
+    {
+        public void Run(Server server)
+        {
+            MvcContext.Get.AssemblyName = Assembly.GetEntryAssembly().GetName().Name;
+            try
+            {
+                server.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}

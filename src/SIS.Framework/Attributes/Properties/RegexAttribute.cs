add55b021c667fbeb7c82f1e35821db52ca07b42
jicata <jitza_nest@yahoo.com>
2018-10-15 17:38:16 +0300

    Added Regex Attribute

---
+++ src/SIS.Framework/Attributes/Properties/RegexAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SIS.Framework.Attributes.Properties
+{
+    public class NumberRangeAttribute : ValidationAttribute
+    {
+        private readonly double minimumValue;
+        private readonly double maximumValue;
+
+        public NumberRangeAttribute(
+            double minimumValue,
+            double maximumValue)
+        {
+            this.minimumValue = minimumValue;
+            this.maximumValue = maximumValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var valueAsNumber = (double) value;
+            return valueAsNumber >= minimumValue &&
+                valueAsNumber <= maximumValue;
+        }
+    }
+}

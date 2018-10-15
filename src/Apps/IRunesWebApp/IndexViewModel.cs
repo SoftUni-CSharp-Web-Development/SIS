c05a2c8ed64bc90b534b3067d22a86159a8b2e60
jicata <jitza_nest@yahoo.com>
2018-10-15 16:26:30 +0300

    Added IndexViewModel

---
+++ src/Apps/IRunesWebApp/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace IRunesWebApp
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public ICollection<string> Roles { get; set; }
+    }
+}

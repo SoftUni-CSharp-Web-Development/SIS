7a1c8f72326560ee5e1f80c7740e8d1067c18fb5
jicata <jitza_nest@yahoo.com>
2018-10-15 17:38:25 +0300

    Added Model entity

---
+++ src/SIS.Framework/Models/Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SIS.Framework.Models
+{
+    public class Model
+    {
+        private bool? isValid;
+
+        private IDictionary<string, string> modelErrors;
+
+        public bool? IsValid
+        {
+            get => this.isValid;
+            set => this.isValid = this.isValid ?? value;
+        }
+    }
+}

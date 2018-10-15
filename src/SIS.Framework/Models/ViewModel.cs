322bba8e494e34e64f3285131acb73b46a18cc74
jicata <jitza_nest@yahoo.com>
2018-10-15 15:09:06 +0300

    Added ViewModel

---
+++ src/SIS.Framework/Models/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SIS.Framework.Models
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.Data = new Dictionary<string, object>();
+        }
+
+        public IDictionary<string, object> Data { get; set; }
+
+        public object this[string key]
+        {
+            get => this.Data[key];
+            set => this.Data[key] = value;
+        }
+    }
+}

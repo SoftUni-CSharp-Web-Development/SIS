e57eb78ff8f8aa94423616df2d5dc7deb537f4cc
Nikolay Kostov <admin@nikolay.it>
2018-10-16 18:53:06 +0300

    Created tests for the view engine

---
+++ src/SIS.MvcFramework.Tests/ViewEngine/ViewEngineTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using SIS.MvcFramework.ViewEngine;
+using Xunit;
+
+namespace SIS.MvcFramework.Tests.ViewEngine
+{
+    public class ViewEngineTests
+    {
+        [Theory]
+        [InlineData("IfForAndForeach")]
+        [InlineData("ViewWithNoCode")]
+        [InlineData("WorkWithViewModel")]
+        public void RunTestViews(string testViewName)
+        {
+            var viewCode = File.ReadAllText($"TestViews/{testViewName}.html");
+            var expectedResult = File.ReadAllText($"TestViews/{testViewName}.Result.html");
+            IViewEngine viewEngine = new MvcFramework.ViewEngine.ViewEngine();
+            var result = viewEngine.GetHtml(viewCode);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ src/SIS.MvcFramework/ViewEngine/ViewEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace SIS.MvcFramework.ViewEngine
+{
+    public class ViewEngine : IViewEngine
+    {
+        public string GetHtml(string viewCode)
+        {
+            return viewCode;
+        }
+    }
+}

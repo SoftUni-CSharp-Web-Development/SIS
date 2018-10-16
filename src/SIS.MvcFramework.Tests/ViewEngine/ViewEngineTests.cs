39f5525e91ef9e36a0592f8a0bcc64e1bb5ab7fe
Nikolay Kostov <admin@nikolay.it>
2018-10-16 19:21:13 +0300

    Skeleton of the ViewEngine, improved tests

--- src/SIS.MvcFramework.Tests/ViewEngine/ViewEngineTests.cs
+++ src/SIS.MvcFramework.Tests/ViewEngine/ViewEngineTests.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using SIS.MvcFramework.ViewEngine;
 using Xunit;
 
@@ -15,8 +17,20 @@ namespace SIS.MvcFramework.Tests.ViewEngine
             var viewCode = File.ReadAllText($"TestViews/{testViewName}.html");
             var expectedResult = File.ReadAllText($"TestViews/{testViewName}.Result.html");
             IViewEngine viewEngine = new MvcFramework.ViewEngine.ViewEngine();
-            var result = viewEngine.GetHtml(viewCode);
+            var model = new TestModel
+            {
+                String = "Username",
+                List = new List<string> { "Item1", "item2", "test", "123", "" }
+            };
+            var result = viewEngine.GetHtml(testViewName, viewCode, model);
             Assert.Equal(expectedResult, result);
+        }
+
+        public class TestModel
+        {
+            public string String { get; set; }
+
+            public IEnumerable<string> List { get; set; }
         }
     }
 }
--- src/SIS.MvcFramework/ViewEngine/ViewEngine.cs
+++ src/SIS.MvcFramework/ViewEngine/ViewEngine.cs
@@ -2,9 +2,41 @@
 {
     public class ViewEngine : IViewEngine
     {
-        public string GetHtml(string viewCode)
+        public string GetHtml<T>(string viewName, string viewCode, T model)
         {
-            return viewCode;
+            var csharpMethodBody = this.GenerateCSharpMethodBody(viewCode);
+            var viewCodeAsCSharpCode = @"
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+namespace MyAppViews
+{
+    public class " + viewName + @"View : IView<" + typeof(T).FullName + @">
+    {
+        public string GetHtml(" + typeof(T).FullName + @" model)
+        {
+            StringBuilder html = new StringBuilder();
+
+            " + csharpMethodBody + @"
+
+            return html.ToString();
+        }
+    }
+}";
+            var instanceOfViewClass = this.GetInstance(viewCodeAsCSharpCode) as IView<T>;
+            var html = instanceOfViewClass.GetHtml(model);
+            return html;
+        }
+
+        private object GetInstance(string cSharpCode)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string GenerateCSharpMethodBody(string viewCode)
+        {
+            return string.Empty;
         }
     }
 }

d92922ad92cb0ef73e28e7a913586296f2f52e68
jicata <jitza_nest@yahoo.com>
2018-10-18 17:36:37 +0300

    Modified Users Controller to make use of Framework

--- src/Apps/IRunesWebApp/Controllers/UsersController.cs
+++ src/Apps/IRunesWebApp/Controllers/UsersController.cs
@@ -1,107 +1,106 @@
-﻿using System;
-using System.Linq;
-using IRunesWebApp.Models;
-using Services;
-using SIS.HTTP.Cookies;
-using SIS.HTTP.Enums;
-using SIS.HTTP.Requests;
-using SIS.HTTP.Responses;
-using SIS.WebServer.Results;
+﻿using IRunesWebApp.Services.Contracts;
+using IRunesWebApp.ViewModels;
+using SIS.Framework.ActionsResults.Base;
+using SIS.Framework.Attributes.Methods;
+using SIS.Framework.Controllers;
 
 namespace IRunesWebApp.Controllers
 {
-    public class UsersController : BaseController
+    public class UsersController : Controller
     {
-        private readonly HashService hashService;
+        private readonly IUsersService usersService;
 
-        public UsersController()
+        public UsersController(IUsersService usersService)
         {
-            this.hashService = new HashService();
+            this.usersService = usersService;
         }
 
-        public IHttpResponse Login(IHttpRequest request) => this.ViewMethod();
+        public IActionResult Login() => this.View();
      
-        public IHttpResponse PostLogin(IHttpRequest request)
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
         {
-            var username = request.FormData["username"].ToString();
-            var password = request.FormData["password"].ToString();
-
-            var hashedPassword = this.hashService.Hash(password);
-
-            var user = this.Context.Users
-                .FirstOrDefault(u => u.Username == username &&
-                    u.HashedPassword == hashedPassword);
-
-            if (user == null)
+            if (!ModelState.IsValid.HasValue || !ModelState.IsValid.Value)
             {
-                return new RedirectResult("/login");
+                return this.RedirectToAction("/users/login");
             }
 
-            var response = new RedirectResult("/home/index");
-            this.SignInUser(username, response, request);
-            return response;
+            var userExists = this.usersService
+                .ExistsByUsernameAndPassword(
+                    model.Username,
+                    model.Password);
+
+            if (!userExists)
+            {
+                return this.RedirectToAction("/users/login");
+            }
+
+            //var response = new RedirectResult("/home/index");
+            //this.SignInUser(username, response, request);
+            this.Request.Session.AddParameter("username", model.Username);
+            return this.RedirectToAction("/home/index");
         }
 
-        public IHttpResponse Register(IHttpRequest request) => this.ViewMethod();
+        //public IHttpResponse Register(IHttpRequest request) => this.ViewMethod();
 
-        public IHttpResponse PostRegister(IHttpRequest request)
-        {
-            var userName = request.FormData["username"].ToString().Trim();
-            var password = request.FormData["password"].ToString();
-            var confirmPassword = request.FormData["confirmPassword"].ToString();
+        //public IHttpResponse PostRegister(IHttpRequest request)
+        //{
+        //    var userName = request.FormData["username"].ToString().Trim();
+        //    var password = request.FormData["password"].ToString();
+        //    var confirmPassword = request.FormData["confirmPassword"].ToString();
 
-            // Validate
-            //if (string.IsNullOrWhiteSpace(userName) || userName.Length < 4)
-            //{
-            //    return new BadRequestResult("Please provide valid username with length of 4 or more characters.");
-            //}
+        //    // Validate
+        //    //if (string.IsNullOrWhiteSpace(userName) || userName.Length < 4)
+        //    //{
+        //    //    return new BadRequestResult("Please provide valid username with length of 4 or more characters.");
+        //    //}
 
-            //if (this.Context.Users.Any(x => x.Username == userName))
-            //{
-            //    return new BadRequestResult("User with the same name already exists.");
-            //}
+        //    //if (this.Context.Users.Any(x => x.Username == userName))
+        //    //{
+        //    //    return new BadRequestResult("User with the same name already exists.");
+        //    //}
 
-            //if (string.IsNullOrWhiteSpace(password) || password.Length < 6)
-            //{
-            //    return new BadRequestResult("Please provide password of length 6 or more.");
-            //}
+        //    //if (string.IsNullOrWhiteSpace(password) || password.Length < 6)
+        //    //{
+        //    //    return new BadRequestResult("Please provide password of length 6 or more.");
+        //    //}
 
-            if (password != confirmPassword)
-            {
-                return new BadRequestResult(
-                    "Passwords do not match.",
-                    HttpResponseStatusCode.SeeOther);
-            }
+        //    if (password != confirmPassword)
+        //    {
+        //        return new BadRequestResult(
+        //            "Passwords do not match.",
+        //            HttpResponseStatusCode.SeeOther);
+        //    }
 
-            // Hash password
-            var hashedPassword = this.hashService.Hash(password);
+        //    // Hash password
+        //    var hashedPassword = this.hashService.Hash(password);
 
-            // Create user
-            var user = new User
-            {
-                Username = userName,
-                HashedPassword = hashedPassword,
-            };
-            this.Context.Users.Add(user);
+        //    // Create user
+        //    var user = new User
+        //    {
+        //        Username = userName,
+        //        HashedPassword = hashedPassword,
+        //    };
+        //    this.Context.Users.Add(user);
 
-            try
-            {
-                this.Context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                // TODO: Log error
-                return new BadRequestResult(
-                    e.Message,
-                    HttpResponseStatusCode.InternalServerError);
-            }
+        //    try
+        //    {
+        //        this.Context.SaveChanges();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        // TODO: Log error
+        //        return new BadRequestResult(
+        //            e.Message,
+        //            HttpResponseStatusCode.InternalServerError);
+        //    }
 
-            var response = new RedirectResult("/");
-            this.SignInUser(userName, response, request);
+        //    var response = new RedirectResult("/");
+        //    this.SignInUser(userName, response, request);
 
-            // Redirect
-            return response;
-        }
+        //    // Redirect
+        //    return response;
+        //}
 
 
     }

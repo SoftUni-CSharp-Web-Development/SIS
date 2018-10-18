aaa0774dd4a7513d18b3244771b6de1adfe29f65
jicata <jitza_nest@yahoo.com>
2018-10-18 16:44:02 +0300

    Added HashingService

--- src/Apps/IRunesWebApp/Controllers/HomeController.cs
+++ src/Apps/IRunesWebApp/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
-﻿using SIS.Framework.ActionsResults.Base;
-using SIS.HTTP.Enums;
-using SIS.HTTP.Requests;
-using SIS.HTTP.Responses;
-using SIS.WebServer.Results;
+﻿using System;
+using Services;
+using SIS.Framework.ActionsResults.Base;
+using SIS.Framework.Controllers;
 
 namespace IRunesWebApp.Controllers
 {
-    public class HomeController : BaseController
+    public class HomeController : Controller
     {
+        private readonly IHashService hashService;
+
+        public HomeController(IHashService hashService)
+        {
+            this.hashService = hashService;
+        }
+
+        public HomeController()
+        {
+            
+        }
+
         public IActionResult Index(IndexViewModel model)
         {
+            var hashedUsername = this.hashService.Hash(model.Username);
+            Console.WriteLine(hashedUsername);
             return this.View();
         }
     }

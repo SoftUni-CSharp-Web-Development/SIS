9684a21911b615769670cbcdbaf717ad4f97eb1c
jicata <jitza_nest@yahoo.com>
2018-10-18 17:38:15 +0300

    Registered additional dependencies FINAL COMMIT FOR EXERCISE: EXTENDING MVC FRAMEWORK - INVERSION OF CONTROL

--- src/Apps/IRunesWebApp/Program.cs
+++ src/Apps/IRunesWebApp/Program.cs
@@ -1,17 +1,14 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using IRunesWebApp.Controllers;
+using IRunesWebApp.Services;
+using IRunesWebApp.Services.Contracts;
 using Services;
 using SIS.Framework;
 using SIS.Framework.Routers;
 using SIS.Framework.Services;
-using SIS.HTTP.Enums;
 using SIS.WebServer;
-using SIS.WebServer.Api;
-using SIS.WebServer.Results;
-using SIS.WebServer.Routing;
-        
+
 namespace IRunesWebApp
 {
     class Program
@@ -21,6 +18,7 @@ namespace IRunesWebApp
             var dependencyMap = new Dictionary<Type, Type>();            
             var dependencyContainer = new DependencyContainer(dependencyMap);
             dependencyContainer.RegisterDependency<IHashService, HashService>();
+            dependencyContainer.RegisterDependency<IUsersService, UsersService>();
 
             var handlingContext = new HttpRouteHandlingContext(
                 new ControllerRouter(dependencyContainer),
@@ -28,28 +26,6 @@ namespace IRunesWebApp
             Server server = new Server(80, handlingContext);
             var engine = new MvcEngine();
             engine.Run(server);
-        }
-
-        private static void ConfigureRouting(ServerRoutingTable serverRoutingTable)
-        {
-            // GET
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/home/index"] =
-                request => new RedirectResult("/");
-            //serverRoutingTable.Routes[HttpRequestMethod.Get]["/"] =
-            //    request => new HomeController().Index(request);
-            //serverRoutingTable.Routes[HttpRequestMethod.Get]["/users/login"] =
-            //    request => new UsersController().Login(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/users/register"] =
-                request => new UsersController().Register(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/albums/all"] =
-                request => new AlbumsController().All(request);
-
-
-            // POST
-            serverRoutingTable.Routes[HttpRequestMethod.Post]["/users/login"] =
-                request => new UsersController().PostLogin(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Post]["/users/register"] =
-                request => new UsersController().PostRegister(request);
         }
     }
 }

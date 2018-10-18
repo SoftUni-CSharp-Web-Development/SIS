276a8db41e08d7e9787803daf5299fc1fda3bdfa
jicata <jitza_nest@yahoo.com>
2018-10-18 16:45:40 +0300

    Added dependency mappings and dependency container initialization

--- src/Apps/IRunesWebApp/Program.cs
+++ src/Apps/IRunesWebApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using IRunesWebApp.Controllers;
+using Services;
 using SIS.Framework;
 using SIS.Framework.Routers;
+using SIS.Framework.Services;
 using SIS.HTTP.Enums;
 using SIS.WebServer;
 using SIS.WebServer.Api;
@@ -14,8 +18,12 @@ namespace IRunesWebApp
     {
         static void Main(string[] args)
         {
+            var dependencyMap = new Dictionary<Type, Type>();            
+            var dependencyContainer = new DependencyContainer(dependencyMap);
+            dependencyContainer.RegisterDependency<IHashService, HashService>();
+
             var handlingContext = new HttpRouteHandlingContext(
-                new ControllerRouter(),
+                new ControllerRouter(dependencyContainer),
                 new ResourceRouter());
             Server server = new Server(80, handlingContext);
             var engine = new MvcEngine();

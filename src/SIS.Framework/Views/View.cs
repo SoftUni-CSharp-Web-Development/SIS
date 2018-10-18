db6a7165e75057fdeae7a9af01377dc6e10bb37e
jicata <jitza_nest@yahoo.com>
2018-10-18 15:09:02 +0300

    Extended View functionality to render layout

--- src/SIS.Framework/Views/View.cs
+++ src/SIS.Framework/Views/View.cs
@@ -2,11 +2,14 @@
 using System.IO;
 using System.Linq;
 using SIS.Framework.ActionsResults.Contracts;
+using SIS.HTTP.Common;
 
 namespace SIS.Framework.Views
 {
     public class View : IRenderable
     {
+        private const string RenderBodyConstant = "@RenderBody()";
+
         private readonly string fullyQualifiedTemplateName;
 
         private readonly IDictionary<string, object> viewData;
@@ -32,7 +35,30 @@ namespace SIS.Framework.Views
             var fullHtml = this.ReadFile();
             var renderedHtml = this.RenderHtml(fullHtml);
 
-            return renderedHtml;
+            var layoutWithView = this.AddViewToLayout(renderedHtml);
+
+            return layoutWithView;
+        }
+
+        private string AddViewToLayout(string renderedHtml)
+        {
+            var layoutViewPath = MvcContext.Get.RootDirectoryRelativePath +
+                GlobalConstants.DirectorySeparator +
+                MvcContext.Get.ViewsFolderName +
+                GlobalConstants.DirectorySeparator +
+                MvcContext.Get.LayoutViewName +
+                GlobalConstants.HtmlFileExtension;
+
+            if (!File.Exists(layoutViewPath))
+            {
+                throw new FileNotFoundException($"View does not exist at {fullyQualifiedTemplateName}");
+            }
+
+            var layoutViewContent = File.ReadAllText(layoutViewPath);
+            var layoutWithView = layoutViewContent.Replace(RenderBodyConstant, renderedHtml);
+
+            return layoutWithView;
+
         }
 
         private string RenderHtml(string fullHtml)

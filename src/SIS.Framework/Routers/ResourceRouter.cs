f8eff734c46842c3a1a3c05ec119dd8373a5a219
jicata <jitza_nest@yahoo.com>
2018-10-18 15:05:34 +0300

    Implemented ResourceRouter

---
+++ src/SIS.Framework/Routers/ResourceRouter.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.WebServer.Api.Contracts;
+using SIS.WebServer.Results;
+
+namespace SIS.Framework.Routers
+{
+    public class ResourceRouter : IHttpHandler
+    {
+        public IHttpResponse Handle(IHttpRequest request)
+        {
+            var httpRequestPath = request.Path;
+
+            var indexOfStartOfExtension = httpRequestPath.LastIndexOf('.');
+            var indexOfStartOfNameOfResource = httpRequestPath.LastIndexOf('/');
+            // users/login/bootstrap.css
+
+            var requestPathExtension = httpRequestPath
+                .Substring(indexOfStartOfExtension);
+
+            var resourceName = httpRequestPath
+                .Substring(
+                    indexOfStartOfNameOfResource);
+
+            var resourcePath = MvcContext.Get.RootDirectoryRelativePath
+                               + $"/{MvcContext.Get.ResourceFolderName}"
+                               + $"/{requestPathExtension.Substring(1)}"
+                               + resourceName;
+
+            if (!File.Exists(resourcePath))
+            {
+                return new HttpResponse(HttpResponseStatusCode.NotFound);
+            }
+
+            var fileContent = File.ReadAllBytes(resourcePath);
+
+            return new InlineResouceResult(fileContent, HttpResponseStatusCode.Ok);
+        }
+    }
+}

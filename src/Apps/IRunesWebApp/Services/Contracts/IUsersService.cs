062131981b1bbf91db0b3031be54f7c16c58e9f3
jicata <jitza_nest@yahoo.com>
2018-10-18 17:35:01 +0300

    Added UsersService

---
+++ src/Apps/IRunesWebApp/Services/Contracts/IUsersService.cs
@@ -0,0 +1,7 @@
+﻿namespace IRunesWebApp.Services.Contracts
+{
+    public interface IUsersService
+    {
+        bool ExistsByUsernameAndPassword(string username, string password);
+    }
+}
---
+++ src/Apps/IRunesWebApp/Services/UsersService.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using IRunesWebApp.Data;
+using IRunesWebApp.Services.Contracts;
+using Services;
+
+namespace IRunesWebApp.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IRunesContext context;
+        private readonly IHashService hashService;
+
+        public UsersService(IRunesContext context, IHashService hashService)
+        {
+            this.context = context;
+            this.hashService = hashService;
+        }
+
+        public bool ExistsByUsernameAndPassword(string username, string password)
+        {
+            var hashedPassword = this.hashService.Hash(password);
+
+            var userExists = this.context.Users
+                .Any(u => u.Username == username &&
+                          u.HashedPassword == hashedPassword);
+
+            return userExists;
+        }
+    }
+}

a0f11bae8149c72e543af936295bd15edd33b1ad
Nikolay Kostov <admin@nikolay.it>
2018-10-19 18:43:07 +0300

    Implemented cakes profile page

---
+++ src/Apps/CakesWebApp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CakesWebApp.ViewModels.User;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+
+namespace CakesWebApp.Controllers
+{
+    public class UserController : BaseController
+    {
+        [HttpGet("/user/profile")]
+        public IHttpResponse Profile()
+        {
+            var viewModel = this.Db.Users.Where(x => x.Username == this.User)
+                .Select(x => new ProfileViewModel
+                             {
+                                 Username = x.Username,
+                                 RegisteredOn = x.DateOfRegistration,
+                                 OrdersCount = x.Orders.Count(),
+                             }).FirstOrDefault();
+
+            if (viewModel == null)
+            {
+                return this.BadRequestError("Profile page not accessible for this user.");
+            }
+
+            return this.View("Profile", viewModel);
+        }
+    }
+}
---
+++ src/Apps/CakesWebApp/ViewModels/User/ProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CakesWebApp.ViewModels.User
+{
+    public class ProfileViewModel
+    {
+        public string Username { get; set; }
+
+        public DateTime RegisteredOn { get; set; }
+
+        public int OrdersCount { get; set; }
+    }
+}

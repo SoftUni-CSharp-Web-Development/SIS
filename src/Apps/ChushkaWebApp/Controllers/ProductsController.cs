92eaee670e19d634acd091c7a22bf18a511a064d
Nikolay Kostov <admin@nikolay.it>
2018-10-30 20:45:23 +0200

    Order details and ordering ready in ChushkaWebApp

---
+++ src/Apps/ChushkaWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChushkaWebApp.Models;
+using ChushkaWebApp.ViewModels.Products;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+
+namespace ChushkaWebApp.Controllers
+{
+    public class ProductsController : BaseController
+    {
+        [Authorize]
+        public IHttpResponse Details(int id)
+        {
+            var viewModel = this.Db.Products
+                .Select(x => new ProductDetailsViewModel
+                {
+                    Type = x.Type,
+                    Name = x.Name,
+                    Id = x.Id,
+                    Price = x.Price,
+                    Description = x.Description,
+                })
+                .FirstOrDefault(x => x.Id == id);
+            if (viewModel == null)
+            {
+                return this.BadRequestError("Invalid product id.");
+            }
+
+            return this.View(viewModel);
+        }
+
+        [Authorize]
+        public IHttpResponse Order(int id)
+        {
+            var user = this.Db.Users.FirstOrDefault(x => x.Username == this.User.Username);
+            if (user == null)
+            {
+                return this.BadRequestError("Invalid user.");
+            }
+
+            var order = new Order
+            {
+                OrderedOn = DateTime.UtcNow,
+                ProductId = id,
+                UserId = user.Id,
+            };
+
+            this.Db.Orders.Add(order);
+            this.Db.SaveChanges();
+
+            return this.Redirect("/");
+        }
+    }
+}
---
+++ src/Apps/ChushkaWebApp/ViewModels/Products/ProductDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChushkaWebApp.Models;
+
+namespace ChushkaWebApp.ViewModels.Products
+{
+    public class ProductDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ProductType Type { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
